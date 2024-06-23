18a54bb78f04c5c72c63abc6cc69e6c16b453aff
Hernan <hernan.alejo.2001@gmail.com>
2024-06-22 22:38:53 -0500

    App Todo list en flutter

---
+++ README.md
@@ -0,0 +1,18 @@
+# todo_list
+
+Basic TODO list. Adding item, remove item etc.
+
+![Flutter demo todolist app](https://imgur.com/DwWUoFx.gif)
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Todo List App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _todoController = TextEditingController();
+  // final TextEditingController _descriptionController = TextEditingController();
+
+  final List<String> _todoList = [
+    "Get Milk",
+    "Get Bread",
+    "Get Eggs",
+    "Get Sleep"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                  controller: _todoController,
+                  decoration: const InputDecoration(
+                      labelText: "Escribe tu tarea", border: OutlineInputBorder())),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _todoList.add(_todoController.text);
+                  });
+                },
+                child: const Text("Add Todo")),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todoList.length,
+              itemBuilder: (context, index) => Card(
+                child: ListTile(
+                    title: Text(_todoList[index]),
+                    subtitle: Text('Item #$index'),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        setState(() {
+                          _todoList.removeAt(index);
+                        });
+                      },
+                    )),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
